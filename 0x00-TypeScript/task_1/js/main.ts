b4b9391a40bf7b108d5921b67d0cb4a097fc181a
Carlos Garcia <agzsoftsi@gmail.com>
2021-02-10 12:04:40 -0500

    Update main.ts

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -1,38 +1,39 @@
 interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [propName: string]: any;
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  [propName: string]: any
 }
 
-
-interface Directors extends Teacher {
+interface Directors extends Teacher{
   numberOfReports: number;
 }
 
+// printing teachers
 interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+  (firstName: string, lastName: string) : string;
 }
 
-export const printTeacher: printTeacherFunction = function (
-  firstName: string,
-  lastName: string
-): string {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName[0]}. ${lastName}`
+}
 
 interface StudentConstructor {
-  new (firstName: string, lastName: string): StudentClassInterface;
+  new(firstName: string, lastName: string): StudentClassInterface;
 }
 
+
 interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
   workOnHomework(): string;
   displayName(): string;
 }
 
-export class StudentClass implements StudentClassInterface {
+export const StudentClass: StudentConstructor =
+class StudentClass implements StudentClassInterface {
   firstName: string;
   lastName: string;
 
@@ -42,7 +43,7 @@ export class StudentClass implements StudentClassInterface {
   }
 
   workOnHomework(): string {
-    return "Currently working";
+    return 'Currently working';
   }
 
   displayName(): string {
