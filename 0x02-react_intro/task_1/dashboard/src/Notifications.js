de01c6c2b0fbc852b40902bf474171d683bf91b4
Carlos Garcia <agzsoftsi@gmail.com>
2021-02-16 11:53:20 -0500

    Create the notification component

---
+++ 0x02-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Notifications.css';
+
+//The function should return a div with the class Notifications
+//The div should contain a paragraph with the text Here is the list of notifications
+export const Notification = () => {
+    return (
+      <div className='Notifications'>
+        <p>Here is the list of notifications</p>
+      </div>
+    );
+  };
+  
+  export default Notification;
